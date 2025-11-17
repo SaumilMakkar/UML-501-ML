e0ac6f027954496b661d85ef0c0fda9cba13342a
Saumil Makkar <saumilmakkar@gmail.com>
2025-11-17 18:01:31 +0530

    "done"

--- ML Project/ML-Project/Model/app.py
+++ ML Project/ML-Project/Model/app.py
@@ -293,13 +293,13 @@ if 'model_accuracies' not in st.session_state:
 
 # Helper function to clean resume text
 def cleanResume(resumeText):
-    resumeText = re.sub('http\S+\s*', ' ', resumeText)
+    resumeText = re.sub(r'http\S+\s*', ' ', resumeText)
     resumeText = re.sub('RT|cc', ' ', resumeText)
-    resumeText = re.sub('#\S+', '', resumeText)
-    resumeText = re.sub('@\S+', '  ', resumeText)
+    resumeText = re.sub(r'#\S+', '', resumeText)
+    resumeText = re.sub(r'@\S+', '  ', resumeText)
     resumeText = re.sub('[%s]' % re.escape("""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""), ' ', resumeText)
     resumeText = re.sub(r'[^\x00-\x7f]', r' ', resumeText)
-    resumeText = re.sub('\s+', ' ', resumeText)
+    resumeText = re.sub(r'\s+', ' ', resumeText)
     return resumeText
 
 # Extract text from PDF
